4ac5fc73f31df822ae8a506e868674c37eba3d22
skeletonk10 <poohbilly2@naver.com>
2023-09-03 05:36:10 +0900

    Use socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import app from "./server";
+import express from "express";
+import cors from 'cors';
+import { Server } from 'socket.io';
 
-app.listen("8000", () => {
+const app = express();
+
+app.use(cors());
+
+const httpServer = app.listen("8000", () => {
   console.log("Server listening on port 8000");
+});
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+  
+  socket.on('message', (data) => {
+    console.log(`User message: ${data}`);
+  });
 });
